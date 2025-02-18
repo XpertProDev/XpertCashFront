d273e95156e049b4073b86e68cb92347ed456f44
apple <xpertproinformatique@gmail.com>
2025-02-17 16:23:51 -0800

    headerComponent

---
+++ src/app/admin-page/Navigation/header-nav/header-nav.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header-nav',
+  imports: [],
+  templateUrl: './header-nav.component.html',
+  styleUrl: './header-nav.component.scss'
+})
+export class HeaderNavComponent {
+
+  constructor(private router: Router) {}
+
+
+goToInscription() {
+  this.router.navigate(['/inscription']);
+}
+
+}
