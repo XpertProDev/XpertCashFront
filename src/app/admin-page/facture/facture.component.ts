b3cdade95c95ee814c2a6958051ed252bdd7dfc2
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-13 15:45:14 -0700

    Jai gerer la pagination de la page facture sur les produits

--- src/app/admin-page/facture/facture.component.ts
+++ src/app/admin-page/facture/facture.component.ts
@@ -1,6 +1,6 @@
 // import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AfterViewInit, ChangeDetectionStrategy, Component, HostListener, signal, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, signal, ViewChild} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -17,6 +17,10 @@ import { StockService } from '../SERVICES/stocks.service';
 import { UsersService } from '../SERVICES/users.service';
 import { Produit } from '../MODELS/produit.model';
 import { Stock } from '../MODELS/stock.model';
+import { Facture, FactureWithDataSource, ProduitFacture } from '../MODELS/facture.model';
+import { FactureService } from '../SERVICES/facture.service';
+import { ViewChildren, QueryList } from '@angular/core';
+
 
 
 export interface PeriodicElement {
@@ -28,11 +32,11 @@ export interface PeriodicElement {
   stockApres: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {codeGenerique: 'P-0000', nomProduit: 'NIDO', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
-  {codeGenerique: 'P-0001', nomProduit: 'Bic', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
-  {codeGenerique: 'P-0002', nomProduit: 'Chocolat', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {codeGenerique: 'P-0000', nomProduit: 'NIDO', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
+//   {codeGenerique: 'P-0001', nomProduit: 'Bic', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
+//   {codeGenerique: 'P-0002', nomProduit: 'Chocolat', stockActuel: 200, quantiteAjoute: 89, quantiteRetirer: 20, stockApres: 234},
+// ];
 
 @Component({
   selector: 'app-facture',
@@ -61,6 +65,10 @@ export class FactureComponent  implements AfterViewInit {
   email: string = '';
   produit: Produit | undefined;
   stockHistory: any[] = [];
+  // Les Facture 
+  // factures: Facture[] = [];
+
+  factures: FactureWithDataSource[] = [];
 
    // Dropdown pour l'export
    showExportDropdown = false;
@@ -70,6 +78,8 @@ export class FactureComponent  implements AfterViewInit {
    adjustedStocks: Stock[] = [];
    stock: Stock | null = null
 
+   factureDataSource = new MatTableDataSource<any>();
+
    constructor(
          private userService: UsersService,
          private produitService: ProduitService,
@@ -77,16 +87,31 @@ export class FactureComponent  implements AfterViewInit {
          private stockService: StockService,
          private router: Router,
          private route: ActivatedRoute,
-       ) {}
+         private factureService: FactureService,
+         private changeDetectorRef: ChangeDetectorRef,
+       ) {
+        this.dataSource = new MatTableDataSource<ProduitFacture>();
+       }
 
   // Pagination
-  displayedColumns: string[] = ['codeGenerique','nomProduit', 'stockActuel', 'quantiteAjoute', 'quantiteRetirer', 'stockApres'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['codeGenerique', 'nomProduit', 'prixUnitair', 'quantite', 'total'];
+  dataSource: MatTableDataSource<ProduitFacture>;
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChildren(MatPaginator) paginators!: QueryList<MatPaginator>;
+  
+
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    this.paginators.changes.subscribe(() => {
+      // Associe chaque paginator à la dataSource de la facture correspondante
+      this.factures.forEach((facture, index) => {
+        const paginator = this.paginators.toArray()[index];
+        if (facture.dataSource && paginator) {
+          facture.dataSource.paginator = paginator;
+        }
+      });
+    }); 
   }
 
   // Collaste 
@@ -96,15 +121,16 @@ export class FactureComponent  implements AfterViewInit {
     this.step.set(index);
   }
 
-  nextStep() {
-    this.step.update(i => i + 1);
-  }
+  // nextStep() {
+  //   this.step.update(i => i + 1);
+  // }
 
-  prevStep() {
-    this.step.update(i => i - 1);
-  }
+  // prevStep() {
+  //   this.step.update(i => i - 1);
+  // }
 
   // Affichage/Masquage du dropdown d'export
+  
   toggleExportDropdown(event: Event) {
     event.stopPropagation(); 
     this.showExportDropdown = !this.showExportDropdown;
@@ -128,6 +154,8 @@ export class FactureComponent  implements AfterViewInit {
   ngOnInit(): void {
     this.getBoutiqueName();
     this.getPartageInfoUser();
+    // List des factures 
+    this.loadFactures();
   }
 
   // Boutique Name
@@ -158,7 +186,41 @@ export class FactureComponent  implements AfterViewInit {
     });
   }
 
+  // loadFactures() {
+  //   this.factureService.getFactures().subscribe({
+  //     next: (data) => {
+  //       this.factures = data.reverse();
+  //       this.changeDetectorRef.markForCheck();
+  //     },
+  //     error: (error) => {
+  //       console.error('Erreur lors de la récupération des factures', error);
+  //     }
+  //   });
+  // }
 
+  loadFactures() {
+    this.factureService.getFactures().subscribe({
+      next: (data) => {
+        this.factures = data.reverse().map(facture => ({
+          ...facture,
+          dataSource: new MatTableDataSource(this.getFormattedProduits(facture))
+        })) as FactureWithDataSource[];
+        this.changeDetectorRef.markForCheck();
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des factures', error);
+      }
+    });
+  }  
+
+  // Méthode pour obtenir les produits formatés
+  getFormattedProduits(facture: Facture): any[] {
+    return facture.produits.map(p => ({
+      ...p,
+      quantiteAjoute: facture.type === 'AJOUT' ? p.quantite : 0,
+      quantiteRetirer: facture.type === 'RETRAIT' ? p.quantite : 0
+    }));
+  }
   
 
 }
