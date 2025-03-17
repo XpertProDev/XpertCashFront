dcea7f6b7882548e90716a2415246694f4e9cdc4
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-17 09:24:15 -0700

    Regler le pagination

--- src/app/admin-page/profil/profil.component.ts
+++ src/app/admin-page/profil/profil.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { CodeInputModule } from 'angular-code-input';
 
 @Component({
   selector: 'app-profil',
   imports: [
-    CodeInputModule
   ],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss'
 })
 export class ProfilComponent {
 
-    // this called every time when user changed the code
-  onCodeChanged(code: string) {
-  }
-
-  // this called only if user entered full code
-  onCodeCompleted(code: string) {
-  }
-
+  
 }
