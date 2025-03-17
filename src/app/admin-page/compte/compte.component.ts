98f35c043eb84a69a5b75a8fd9d56e538e44c0e9
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-17 16:42:26 -0700

    creation de page compte

--- src/app/admin-page/compte/compte.component.ts
+++ src/app/admin-page/compte/compte.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-compte',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './compte.component.html',
   styleUrl: './compte.component.scss'
 })
