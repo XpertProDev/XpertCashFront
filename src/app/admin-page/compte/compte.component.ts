58de26516c0e5df54c74d7c434156f0aa44326e3
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-18 12:15:06 -0700

    Lister les roles

--- src/app/admin-page/compte/compte.component.ts
+++ src/app/admin-page/compte/compte.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Roles } from '../MODELS/roles.model';
+import { RolesService } from '../SERVICES/roles.service';
 
 @Component({
   selector: 'app-compte',
   imports: [
-    RouterLink
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './compte.component.html',
   styleUrl: './compte.component.scss'
 })
 export class CompteComponent {
+
+  showPopup = false;
+  roles: Roles[] = [];
+  selectedRole: string = '';
+
+  constructor(private rolesService: RolesService) {}
+
+  ngOnInit() {
+    this.loadRoles();
+  }
+
+  loadRoles() {
+    const token = localStorage.getItem('authToken'); 
+    if (token) {
+      this.rolesService.getAllRoles(token).subscribe({
+        next: (data) => {
+          this.roles = data;
+          console.log('Rôles récupérés:', data);
+        },
+        error: (err) => {
+          console.error('Erreur lors du chargement des rôles', err);
+        }
+      });
+    }
+  }
+
+  openPopup() {
+    this.showPopup = true;
+  }
+
+  closePopup() {
+    this.showPopup = false;
+  }
 
 }
