faa1384ea5eebdf26287ddbd6e1f65c06b91c1c8
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-21 16:12:59 -0700

    CustomNumero Pipe

--- src/app/admin-page/MODELS/customNumberPipe.ts
+++ src/app/admin-page/MODELS/customNumberPipe.ts
@@ -7,6 +7,6 @@ export class CustomNumberPipe implements PipeTransform {
   transform(value: number): string {
     if (value == null) return '';
     // Remplacer les milliers par des points
-    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
   }
 }
