77212228437c2bca1a7aa3e41b59626a102c4511
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-03-25 13:30:44 -0700

    page tranfert implementation edit

--- src/app/admin-page/SERVICES/transfert-service.ts
+++ src/app/admin-page/SERVICES/transfert-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Transfert } from "../MODELS/tranfert-model";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -13,13 +14,13 @@ import { Transfert } from "../MODELS/tranfert-model";
   constructor(private http: HttpClient) { }
 
   // Dans transfert.service.ts
-    effectuerTransferts(transferts: Transfert[]) {
-        const token = localStorage.getItem('authToken') || '';
-        const headers = new HttpHeaders({
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        });
+  effectuerTransfert(transfert: Transfert): Observable<any> {
+    const token = localStorage.getItem('authToken') || '';
+    const headers = new HttpHeaders({
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    });
 
-        return this.http.post(`${this.apiUrl}/transferer-produits`, transferts);
-    }
+    return this.http.post(`${this.apiUrl}/transferer-produits`, transfert, { headers });
+  }
 }
