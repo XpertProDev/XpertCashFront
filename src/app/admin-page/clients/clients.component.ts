b9c6e1e9885f1bd84e60cf235fa39e09047ac030
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-04-07 10:28:12 -0700

    Card et list

--- src/app/admin-page/clients/clients.component.ts
+++ src/app/admin-page/clients/clients.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  imports: [
+      CommonModule,
+  ],
   templateUrl: './clients.component.html',
-  styleUrl: './clients.component.scss'
+  styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent {
 
+  isListView = true; // Par défaut on affiche la liste
+  showDropdown = false; // Contrôle l'affichage du dropdown
+  clients = [ // Déplace tes données dans un tableau
+    { name: 'Koureissi SY', email: 'sydiakaridia00@gmail', address: 'Koulouba', phone: '78711623' },
+    { name: 'Amadou Traore', email: 'Amadou@gmail', address: 'Koulouba', phone: '79632526' },
+    { name: 'Mamoutou Daiby', email: 'Mamoutou@gmail', address: 'Koulouba', phone: '72464400' }
+  ];
+
+  toggleView(viewType: string) {
+    this.isListView = viewType === 'list';
+    this.showDropdown = true;
+  }
 }
