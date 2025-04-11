f4b51c2728376c310d478135c4b76c060ea203f1
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-04-11 16:35:45 -0700

    Je vais commencer la modification de client

--- src/app/admin-page/clients/detail-edit-entreprise-client/detail-edit-entreprise-client.component.ts
+++ src/app/admin-page/clients/detail-edit-entreprise-client/detail-edit-entreprise-client.component.ts
@@ -1,26 +1,42 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ClientService } from '../../SERVICES/client-service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { EntrepriseClientService } from '../../SERVICES/entreprise-clients-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-edit-entreprise-client',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './detail-edit-entreprise-client.component.html',
   styleUrl: './detail-edit-entreprise-client.component.scss'
 })
 export class DetailEditEntrepriseClientComponent {
 
+  entrepriseClientEditForm!: FormGroup;
+  errorMessageApi: string = '';
+  successMessage: string = '';
+  isSubmitting = false;
+  private destroy$ = new Subject<void>();
+
   constructor(
-      // private clientService: ClientService,
       private router: Router,
+      private fb: FormBuilder,
+      private entrepriseClientService: EntrepriseClientService,
   ) {}
   
   ngOnInit() {
     
   }
 
-  // entreprise client id routing
-  openEntrepriseClientDetail(clientId: number): void {
-    this.router.navigate(['/detail-entreprise', clientId]);
+  // Annuler et revenir à la liste
+  goToClients() {
+    this.router.navigate(['/clients']);
   }
+
 }
