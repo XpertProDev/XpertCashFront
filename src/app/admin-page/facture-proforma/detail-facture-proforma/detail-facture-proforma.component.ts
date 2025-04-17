4ce67a6568f1ca105c6dfed4d9e5f5e2d5b36441
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-04-17 08:54:35 -0700

    Detail de facture

--- src/app/admin-page/facture-proforma/detail-facture-proforma/detail-facture-proforma.component.ts
+++ src/app/admin-page/facture-proforma/detail-facture-proforma/detail-facture-proforma.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-facture-proforma',
-  imports: [],
+  imports: [ FormsModule, CommonModule, ReactiveFormsModule,],
   templateUrl: './detail-facture-proforma.component.html',
   styleUrl: './detail-facture-proforma.component.scss'
 })
 export class DetailFactureProformaComponent {
+  isLoading: boolean = false;
 
 }
