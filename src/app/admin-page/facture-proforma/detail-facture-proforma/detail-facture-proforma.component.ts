e7c8de2decfe18d1c0bea0fcf6b8ed2ce041e7f9
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-04-17 10:46:50 -0700

    Detail de facture page

--- src/app/admin-page/facture-proforma/detail-facture-proforma/detail-facture-proforma.component.ts
+++ src/app/admin-page/facture-proforma/detail-facture-proforma/detail-facture-proforma.component.ts
@@ -1,14 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detail-facture-proforma',
-  imports: [ FormsModule, CommonModule, ReactiveFormsModule,],
+  imports: [ FormsModule, CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './detail-facture-proforma.component.html',
   styleUrl: './detail-facture-proforma.component.scss'
 })
-export class DetailFactureProformaComponent {
+export class DetailFactureProformaComponent implements OnInit {
   isLoading: boolean = false;
+  remiseTVAActive: boolean = false;
+  remisePourcentage: number = 0;
+  tva: number = 0;
+  errorMessage: string = '';
+
+
+  ngOnInit(): void {}
+
+  // Toggle remise / TVA
+  onToggleRemiseTVA() {
+    if (!this.remiseTVAActive) {
+      this.remisePourcentage = 0;
+      this.tva = 0;
+    }
+  }
+
+  submitUpdateForm() {}
 
 }
