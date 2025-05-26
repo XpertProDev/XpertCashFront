2277576f9630237f0341463e697d68bca5f71286
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-05-26 16:53:30 -0700

    Ui de setting facture is good

--- src/app/admin-page/account-settings/account-settings.component.ts
+++ src/app/admin-page/account-settings/account-settings.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -23,9 +23,14 @@ import {MatTabsModule} from '@angular/material/tabs';
 export class AccountSettingsComponent implements OnInit {
   panelOpenState = false;
   form!: FormGroup; 
+  // L'URL du logo
+  logoUrl: string = 'assets/img/logo.jpeg';
+  // Référence à l'input file
+  @ViewChild('fileInput') fileInput!: ElementRef;
 
   constructor(
     private fb: FormBuilder,
+    private cdRef: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
@@ -38,7 +43,7 @@ export class AccountSettingsComponent implements OnInit {
       secteur: ['xxxx'],
       email: ['example@gmail.com'],
       adresse: ['xxxx'],
-      pays: ['xxxx'],
+      pays: ['Bamako-Mali'],
       telephone: ['xx xx xx xx'],
       siteWeb: ['www.xpertpro.com'],
       banque: ['xxxx'],
@@ -48,5 +53,43 @@ export class AccountSettingsComponent implements OnInit {
       signataire: ['Monsieur X'],
       post: ['Le Directeur Général']
     });
+  }
+
+  // Gestionnaire de clic pour le lien "ICI"
+  onChangeLogoClick(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  // Gestionnaire de sélection de fichier
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const img = new Image();
+        img.src = e.target.result;
+        
+        img.onload = () => {
+          // Redimensionner l'image si nécessaire
+          const MAX_SIZE = 800;
+          const canvas = document.createElement('canvas');
+          const ctx = canvas.getContext('2d');
+
+          if(img.width > MAX_SIZE || img.height > MAX_SIZE) {
+            const ratio = Math.min(MAX_SIZE/img.width, MAX_SIZE/img.height);
+            canvas.width = img.width * ratio;
+            canvas.height = img.height * ratio;
+            ctx?.drawImage(img, 0, 0, canvas.width, canvas.height);
+            this.logoUrl = canvas.toDataURL('image/jpeg', 0.8);
+          } else {
+            this.logoUrl = e.target.result;
+          }
+
+          this.cdRef.markForCheck();
+          this.fileInput.nativeElement.value = '';
+        };
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
