0c6a5536c1a2bf35fed500031c275a0cfdbcfd7b
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-05-26 16:27:41 -0700

    affichage de ce que on ecrit dans le champ est recu

--- src/app/admin-page/account-settings/account-settings.component.ts
+++ src/app/admin-page/account-settings/account-settings.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,11 +10,43 @@ import {MatTabsModule} from '@angular/material/tabs';
   selector: 'app-account-settings',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatTabsModule, MatExpansionModule, MatFormFieldModule],
+  imports: [
+    MatTabsModule, 
+    MatExpansionModule, 
+    MatFormFieldModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './account-settings.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './account-settings.component.scss'
 })
-export class AccountSettingsComponent {
+export class AccountSettingsComponent implements OnInit {
   panelOpenState = false;
+  form!: FormGroup; 
+
+  constructor(
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.getFormInit();
+  }
+
+  getFormInit() {
+    this.form = this.fb.group({
+      nomEntreprise: ['Entreprise'], 
+      secteur: ['xxxx'],
+      email: ['example@gmail.com'],
+      adresse: ['xxxx'],
+      pays: ['xxxx'],
+      telephone: ['xx xx xx xx'],
+      siteWeb: ['www.xpertpro.com'],
+      banque: ['xxxx'],
+      nina: ['xxxx'],
+      rccm: ['xxxx'],
+      nif: ['xxxx'],
+      signataire: ['Monsieur X'],
+      post: ['Le Directeur Général']
+    });
+  }
 }
