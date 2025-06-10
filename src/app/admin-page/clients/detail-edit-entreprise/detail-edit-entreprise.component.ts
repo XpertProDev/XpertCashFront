ea84eedc46dc01b5c42ca24f997590cd2520d152
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-06-10 09:52:02 -0700

    je vais commencer a implementer la logique de lister et editer

--- src/app/admin-page/clients/detail-edit-entreprise/detail-edit-entreprise.component.ts
+++ src/app/admin-page/clients/detail-edit-entreprise/detail-edit-entreprise.component.ts
@@ -1,11 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-edit-entreprise',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './detail-edit-entreprise.component.html',
   styleUrl: './detail-edit-entreprise.component.scss'
 })
 export class DetailEditEntrepriseComponent {
+
+  entrepriseForm!: FormGroup;
+  errorMessage: string = '';
+  errorMessageApi: string = '';
+  successMessage = '';
+
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private fb: FormBuilder,
+  ) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  getFormEntreprise() {
+    this.entrepriseForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.email]],
+      telephone: ['', [Validators.pattern(/^\d+$/)]],
+      pays: [''],
+      adresse: [''],
+      siege: [''],
+      secteur: ['']
+    });
+  }
+
+  initializeForm() {
+    
+  }
+
+  goToClients() {
+    this.router.navigate(['/clients']);
+  }
+
+  onSubmit() {}
 
 }
