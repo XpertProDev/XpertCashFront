1dabd57fcac5e395123df4c9aadee676a2cc7487
Xpertpro-sy <informatiquexpertpro2@gmail.com>
2025-07-30 13:08:52 -0700

    Dinamiser le button onglet

--- src/app/POS/pos-vente/pos-vente.component.ts
+++ src/app/POS/pos-vente/pos-vente.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { ViewStateService } from '../pos-accueil/view-state.service';
+import { Categorie } from 'src/app/admin-page/MODELS/categorie.model';
+import { CategorieService } from 'src/app/admin-page/SERVICES/categorie.service';
 
 @Component({
   selector: 'app-pos-vente',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './pos-vente.component.html',
   styleUrl: './pos-vente.component.scss'
 })
@@ -15,10 +17,13 @@ export class PosVenteComponent {
   isListView = true;
   showDropdown = false;
   showPaymentPopup = false;
+  categories: Categorie[] = [];
+  selectedCategoryId: number | null = null;
 
   constructor(
     private router: Router,
-    private viewState: ViewStateService
+    private viewState: ViewStateService,
+    private categorieService: CategorieService
   ) {}
 
   ngOnInit() {
@@ -30,6 +35,26 @@ export class PosVenteComponent {
     this.viewState.isListView$.subscribe(view => {
       this.isListView = view;
     });
+
+    // Récupérez les catégories
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.categorieService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+        console.log('Catégories chargées:', this.categories);
+      },
+      error: (error) => {
+        console.error('Erreur lors du chargement des catégories', error);
+      }
+    });
+  }
+
+  selectCategory(categoryId: number) {
+    this.selectedCategoryId = categoryId;
+    // Implémentez ici le filtrage des produits
   }
 
   openPaymentPopup() {
