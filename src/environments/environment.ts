f94a14d4da182cb991465a92b79f8e9fe2fa472a
XpertProDev <xpertproinformatique@gmail.com>
2025-08-28 09:41:28 +0000

    Config Env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,25 @@
 import packageInfo from '../../package.json';
 
 
-// export const environment = {
-//   appVersion: packageInfo.version,
-//   production: false,
-//   apiUrl: 'http://192.168.1.12:4200',
-//   apiBaseUrl: 'http://192.168.1.12:8080/api/auth',
-//   imgUrl: "http://192.168.1.12:8080",
-//   wsEndpoint: 'http://192.168.1.12:8080/ws'
-  
-// };
-
-// localhos
 export const environment = {
   appVersion: packageInfo.version,
   production: false,
-  apiUrl: 'http://localhost:4200',
-  apiBaseUrl: 'http://localhost:8080/api/auth',
-  imgUrl: "http://localhost:8080",
-  wsEndpoint: 'ws://localhost:8080/ws'
+  apiUrl: 'http://192.168.1.11:4200',
+  apiBaseUrl: 'http://192.168.1.11:8080/api/auth',
+  imgUrl: "http://192.168.1.11:8080",
+  wsEndpoint: 'http://192.168.1.11:8080/ws'
+  
 };
+
+// localhos
+// export const environment = {
+//   appVersion: packageInfo.version,
+//   production: false,
+//   apiUrl: 'http://localhost:4200',
+//   apiBaseUrl: 'http://localhost:8080/api/auth',
+//   imgUrl: "http://localhost:8080",
+//   wsEndpoint: 'ws://localhost:8080/ws'
+// };
 
 
 
